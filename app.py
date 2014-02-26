7ee74706e1f0ed71198cce77a847a584ffe829c5
Jesse Gunsch <jesse.gunsch@gmail.com>
2014-02-25 22:48:17 -0700

    Initial commit: hello world app + WSGI.

---
+++ app.py
@@ -0,0 +1,28 @@
+# things to do:
+# One: file-based configuration. Set up mysql connection.
+# Two: finish git deployment setup
+#
+# Note: config settings should include the following built-ins:
+# DEBUG = True
+# SECRET_KEY (?)
+# SQL login info
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/lol")
+def name_isnt_important():
+    return "blah"
+
+@app.errorhandler(500)
+def error_500(error):
+    from traceback import format_exc
+    return format_exc(), 500, {'Content-Type': 'text/plain'}
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ app.wsgi
@@ -0,0 +1,7 @@
+# Python WSGI module to enable starting the application from Apache with mod_wsgi.
+
+import sys
+sys.path.insert(0, '/srv/qxlp.net/brackets')
+
+from app import app as application
+
